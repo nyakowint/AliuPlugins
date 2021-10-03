56449a4b0621f8a17ca802ae7f509878b63413f9
TymanWasTaken <tyman@tyman.tech>
2021-10-03 10:44:37 -0600

    fix false positives

--- PinIcon/build.gradle.kts
+++ PinIcon/build.gradle.kts
@@ -1,9 +1,12 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Shows a pin icon next to messages that have been pinned"
 
 aliucord {
     changelog.set("""
+        # 1.0.1
+        * Fixed false positives - Tyman
         # 1.0.0
-		* Released
+        * Released
     """.trimIndent())
+    author("Tyman", 487443883127472129L)
 }
