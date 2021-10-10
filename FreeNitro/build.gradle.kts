594d9b2e42d5c38105cbfcf4ed54496a10af606c
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-10-09 21:59:50 -0400

    FortNite i mean FreeNitro: chekc for nitro

--- FreeNitro/build.gradle.kts
+++ FreeNitro/build.gradle.kts
@@ -1,8 +1,10 @@
-version = "1.0.1"
+version = "1.0.2"
 description = "This plugin DOES NOT give you ANY nitro features whatsoever. It is entirely intended to troll people who don't read / install plugins without reading what they do."
 
 aliucord {
     changelog.set("""
+        # 1.0.2
+        * im gonna stop updating the changelog cause this is just a joke plugin lol
         # 1.0.1
         * Change message, fix emote
         # 1.0.0
--- FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
+++ FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.api.CommandsAPI
 import com.aliucord.entities.Plugin
+import com.discord.api.premium.PremiumTier
+import com.discord.stores.StoreStream
 
 @AliucordPlugin
 class FreeNitro : Plugin() {
     override fun start(ctx: Context) {
         commands.registerCommand("nitro", "Free nitro! Totally not a scam 10% legit") {
-            CommandsAPI.CommandResult(
-                "I installed the FreeNitro plugin probably expecting nitro features for free, but now know that such things are not possible! What an epic realization <:posttroll:859798445870809108> <:iloveccp:892176962934689822><:iloveccp:892176962934689822> \uD83C\uDDE8\uD83C\uDDF3",
-                null,
-                true
-            )
+            if (StoreStream.getUsers().me.premiumTier == PremiumTier.TIER_1 || StoreStream.getUsers().me.premiumTier == PremiumTier.TIER_2) {
+                CommandsAPI.CommandResult("I installed the FreeNitro plugin and it worked exactly as advertised! Everything worked great without a hitch and I'm quite satisfied! Thanks Aliucord discord server! You guys are okay in my book! ☺ ❤")
+            } else {
+                CommandsAPI.CommandResult(
+                    "I installed the FreeNitro plugin probably expecting nitro features for free, but now know that such things are not possible! What an epic realization <:posttroll:859798445870809108> <:iloveccp:892176962934689822><:iloveccp:892176962934689822> \uD83C\uDDE8\uD83C\uDDF3",
+                    null,
+                    true
+                )
+            }
         }
     }
 
