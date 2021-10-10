94f1d29f8cc964bac18b6a9151c4aa402228772b
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-10-09 21:41:41 -0400

    FN: change message, fix emote

--- FreeNitro/build.gradle.kts
+++ FreeNitro/build.gradle.kts
@@ -1,8 +1,10 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "This plugin DOES NOT give you ANY nitro features whatsoever. It is entirely intended to troll people who don't read / install plugins without reading what they do."
 
 aliucord {
     changelog.set("""
+        # 1.0.1
+        * Change message, fix emote
         # 1.0.0
 		* <:iloveccp:892176962934689822> 
     """.trimIndent())
--- FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
+++ FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
@@ -10,7 +10,7 @@ class FreeNitro : Plugin() {
     override fun start(ctx: Context) {
         commands.registerCommand("nitro", "Free nitro! Totally not a scam 10% legit") {
             CommandsAPI.CommandResult(
-                "I installed the FreeNitro plugin probably expecting free nitro features, but now know that such things are not possible! What an epic realization :posttroll: <:iloveccp:892176962934689822><:iloveccp:892176962934689822> \uD83C\uDDE8\uD83C\uDDF3",
+                "I installed the FreeNitro plugin probably expecting nitro features for free, but now know that such things are not possible! What an epic realization <:posttroll:859798445870809108> <:iloveccp:892176962934689822><:iloveccp:892176962934689822> \uD83C\uDDE8\uD83C\uDDF3",
                 null,
                 true
             )
