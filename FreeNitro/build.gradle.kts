b564a6fc335b2e98242e231efb98ee79a85d27a2
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-10-09 22:29:07 -0400

    "glory to the ccp"

--- FreeNitro/build.gradle.kts
+++ FreeNitro/build.gradle.kts
@@ -1,13 +1,13 @@
-version = "1.0.2"
+version = "1.0.3"
 description = "This plugin DOES NOT give you ANY nitro features whatsoever. It is entirely intended to troll people who don't read / install plugins without reading what they do."
 
 aliucord {
     changelog.set("""
-        # 1.0.2
+        # 1.0.3
         * im gonna stop updating the changelog cause this is just a joke plugin lol
         # 1.0.1
         * Change message, fix emote
         # 1.0.0
-		* <:iloveccp:892176962934689822> 
+		* CCP
     """.trimIndent())
 }
--- FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
+++ FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
@@ -12,7 +12,7 @@ class FreeNitro : Plugin() {
     override fun start(ctx: Context) {
         commands.registerCommand("nitro", "Free nitro! Totally not a scam 10% legit") {
             if (StoreStream.getUsers().me.premiumTier == PremiumTier.TIER_1 || StoreStream.getUsers().me.premiumTier == PremiumTier.TIER_2) {
-                CommandsAPI.CommandResult("I installed the FreeNitro plugin and it worked exactly as advertised! Everything worked great without a hitch and I'm quite satisfied! Thanks Aliucord discord server! You guys are okay in my book! ☺ ❤")
+                CommandsAPI.CommandResult("I installed the FreeNitro plugin and it worked exactly as advertised! Everything worked great without a hitch and I'm quite satisfied! Thanks Aliucord discord server! You guys are okay in my book! ☺ ❤", null, true)
             } else {
                 CommandsAPI.CommandResult(
                     "I installed the FreeNitro plugin probably expecting nitro features for free, but now know that such things are not possible! What an epic realization <:posttroll:859798445870809108> <:iloveccp:892176962934689822><:iloveccp:892176962934689822> \uD83C\uDDE8\uD83C\uDDF3",
