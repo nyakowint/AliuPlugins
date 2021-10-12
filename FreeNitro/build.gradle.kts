2f99ec26214a5df1081199a730a22ac341aeaa89
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-10-12 11:12:10 -0400

    F(ort)N(ite): a billion percent more trolling

--- FreeNitro/build.gradle.kts
+++ FreeNitro/build.gradle.kts
@@ -1,8 +1,10 @@
-version = "1.1.0"
+version = "1.2.0"
 description = "\"\"Free\"\" nitro :^)"
 
 aliucord {
     changelog.set("""
+        # 1.2.0
+        * now includes 600% more epic trolling 
         # 1.1.0
         * Make method better :kapp:
     """.trimIndent())
--- FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
+++ FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
@@ -1,19 +1,44 @@
 package me.aniimalz.plugins
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.widget.TextView
+import com.aliucord.PluginManager
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.api.CommandsAPI
 import com.aliucord.entities.Plugin
+import com.aliucord.patcher.Hook
+import com.aliucord.patcher.InsteadHook
+import com.aliucord.utils.ReflectUtils
 import com.discord.api.premium.PremiumTier
+import com.discord.databinding.WidgetChatListAdapterItemEphemeralMessageBinding
+import com.discord.models.message.Message
 import com.discord.stores.StoreStream
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemEphemeralMessage
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
+import com.discord.widgets.chat.list.entries.ChatListEntry
+import com.discord.widgets.chat.list.entries.EphemeralMessageEntry
+import de.robv.android.xposed.XC_MethodHook
+import top.canyie.pine.callback.MethodReplacement
 
 @AliucordPlugin
 class FreeNitro : Plugin() {
+
+    var tortureMe = false
+
+    @SuppressLint("SetTextI18n")
     override fun start(ctx: Context) {
+
         commands.registerCommand("nitro", "Free nitro! Totally not a scam 10% legit") {
-            if (StoreStream.getUsers().me.premiumTier == PremiumTier.TIER_1 || StoreStream.getUsers().me.premiumTier == PremiumTier.TIER_2) {
-                CommandsAPI.CommandResult("I installed the FreeNitro plugin and it worked exactly as advertised! Everything worked great without a hitch and I'm quite satisfied! Thanks Aliucord discord server! You guys are okay in my book! ☺ ❤", null, true)
+            if (StoreStream.getUsers().me.premiumTier != PremiumTier.NONE) {
+                patcher.unpatchAll()
+                CommandsAPI.CommandResult(
+                    "I installed the FreeNitro plugin and it worked exactly as advertised! Everything worked great without a hitch and I'm quite satisfied! Thanks Aliucord discord server! You guys are okay in my book! ☺ ❤",
+                    null,
+                    true
+                )
             } else {
+                patcher.unpatchAll()
                 CommandsAPI.CommandResult(
                     "I installed the FreeNitro plugin probably expecting nitro features for free, but now know that such things are not possible! What an epic realization <:posttroll:859798445870809108> <:iloveccp:892176962934689822><:iloveccp:892176962934689822> \uD83C\uDDE8\uD83C\uDDF3",
                     null,
@@ -21,9 +46,53 @@ class FreeNitro : Plugin() {
                 )
             }
         }
+
+        commands.registerCommand("tortureme", "Use this to turn the free nitro shit back on") {
+            patcher.unpatchAll()
+            tortureMe = true
+            epicTrolling()
+            CommandsAPI.CommandResult("re-enabled the torture, you're welcome.", null, false)
+        }
+        epicTrolling()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun epicTrolling() {
+        var dont = false
+        patcher.patch(Message::class.java.getDeclaredMethod("getContent"), Hook {
+            if (dont && !tortureMe || listOf(118437263754395652L, 289556910426816513L).contains(StoreStream.getUsers().me.id) && !tortureMe) {
+                patcher.unpatchAll()
+                dont = true
+                return@Hook
+            }
+            val msg = it.thisObject as Message
+            val msgContent = (ReflectUtils.getField(msg, "content") as String)
+            if (StoreStream.getUsers().me.id == 587156686612201482L) {
+                it.result = "Moulz"
+            } else {
+                it.result = "$msgContent sussy"
+            }
+        })
+        patcher.patch(Message::class.java.getDeclaredMethod("getMentionEveryone"), InsteadHook.returnConstant(true))
+        patcher.patch(Message::class.java.getDeclaredMethod("getTts"), InsteadHook.returnConstant(true))
+
+        val ephBinding = WidgetChatListAdapterItemEphemeralMessage::class.java.getDeclaredField("binding").apply { isAccessible = true }
+
+        patcher.patch(WidgetChatListAdapterItemEphemeralMessage::class.java.getDeclaredMethod("onConfigure", Int::class.java, ChatListEntry::class.java), Hook {
+            if (dont && !tortureMe || listOf(118437263754395652L, 289556910426816513L).contains(StoreStream.getUsers().me.id) && !tortureMe) {
+                patcher.unpatchAll()
+                dont = true
+                return@Hook
+            }
+            val binding = ephBinding.get(it.thisObject) as WidgetChatListAdapterItemEphemeralMessageBinding
+            binding.e.text = "aliucord sussy user wtf? dies of cring"
+            if (StoreStream.getUsers().me.id == 587156686612201482L) binding.e.text = "Moulz"
+        })
+        patcher.patch(Message::class.java.getDeclaredMethod("isEphemeralMessage"), InsteadHook.returnConstant(true))
     }
 
     override fun stop(context: Context) {
         commands.unregisterAll()
+        patcher.unpatchAll()
     }
 }
