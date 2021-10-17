1a7464dcce96a7abe022845363fcfedbce89d366
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-10-17 16:00:10 -0400

    Ignore messages with plugin links

--- NoKyzas/build.gradle.kts
+++ NoKyzas/build.gradle.kts
@@ -1,8 +1,10 @@
-version = "1.0.2"
+version = "1.0.3"
 description = "Changes grammatically correct sentences to discord speak"
 
 aliucord {
     changelog.set("""
+        # 1.0.3
+            * Ignores messages with plugin links
         # 1.0.2
             * Everything is lowercase now. ignore yourself to send other stuff
         # 1.0.1
