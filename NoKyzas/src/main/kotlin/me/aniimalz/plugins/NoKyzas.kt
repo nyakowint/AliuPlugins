252b599ac25abde51bfdeaeae744a695732b7922
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-10-17 15:59:21 -0400

    Ignore messages with plugin links

--- NoKyzas/src/main/kotlin/me/aniimalz/plugins/NoKyzas.kt
+++ NoKyzas/src/main/kotlin/me/aniimalz/plugins/NoKyzas.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
+import com.aliucord.Constants
 import com.aliucord.Logger
 import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
@@ -19,6 +20,7 @@ import com.discord.models.user.CoreUser
 import com.discord.stores.StoreStream
 import com.discord.views.CheckedSetting
 import com.lytefast.flexinput.R
+import java.util.regex.Pattern
 
 @AliucordPlugin
 class NoKyzas : Plugin() {
@@ -35,11 +37,17 @@ class NoKyzas : Plugin() {
     }
 
     override fun start(ctx: Context) {
+        // blatantly stolen regex from ven https://github.com/Vendicated/AliucordPlugins/blob/94d7cabc8cb8886cf458ada8617b190aa670c0c3/PluginDownloader/src/main/java/dev/vendicated/aliucordplugs/plugindownloader/PluginDownloader.java#L37
+        val repoPattern = Pattern.compile("https?://github\\.com/([A-Za-z0-9\\-_.]+)/([A-Za-z0-9\\-_.]+)").toRegex()
+        val zipPattern = Pattern.compile("https?://(?:github|raw\\.githubusercontent)\\.com/([A-Za-z0-9\\-_.]+)/([A-Za-z0-9\\-_.]+)/(?:raw|blob)?/?\\w+/(\\w+).zip").toRegex()
         patcher.patch(Message::class.java.getDeclaredMethod("getContent"), Hook {
             if (!settings.getBool("uncap", true)) {
                 return@Hook
             }
-            val msg = (ReflectUtils.getField(it.thisObject as Message, "content") as String)
+            val rMsg = it.thisObject as Message
+            if (rMsg.channelId == Constants.PLUGIN_LINKS_CHANNEL_ID || rMsg.channelId == Constants.PLUGIN_LINKS_UPDATES_CHANNEL_ID) return@Hook
+            val msg = (ReflectUtils.getField(rMsg, "content") as String)
+            if (repoPattern.containsMatchIn(msg) || zipPattern.containsMatchIn(msg)) return@Hook
             if (CoreUser((it.thisObject as Message).author).id == StoreStream.getUsers().me.id && !settings.getBool("replaceSelf", false)) return@Hook
             val result = msg.lowercase()
             it.result = result.dropLastWhile { c -> c == '.' || c == '​' }
