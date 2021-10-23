614911e63be9fee29fd3a919be47bef32723ff9f
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-10-23 16:27:37 -0400

    forgot to version increment

--- FreeNitroll/build.gradle.kts
+++ FreeNitroll/build.gradle.kts
@@ -1,11 +1,2 @@
-version = "1.3.5"
+version = "1.3.6"
 description = "\"\"Free\"\" nitro :^)"
-
-aliucord {
-    changelog.set("""
-        # 1.3.5
-        * increment version for absolutely no reason at all
-        # 1.3.4
-        * More trolling
-    """.trimIndent())
-}
--- NoKyzas/build.gradle.kts
+++ NoKyzas/build.gradle.kts
@@ -1,16 +1,11 @@
-version = "1.0.4"
+version = "1.0.5"
 description = "Changes grammatically correct sentences to discord speak"
 
 aliucord {
     changelog.set("""
+        # 1.0.5
+            * Entirely ignores messages with links to prevent dumb shit like invites breaking
         # 1.0.4
             * Setting to ignore bots
-        # 1.0.3
-            * Ignores messages with plugin links
-        # 1.0.2
-            * Everything is lowercase now. ignore yourself to send other stuff
     """.trimIndent())
 }
-
-/* # 1.1.0
-            * Settings to ignore specific users/channels/servers */
