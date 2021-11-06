e4f1a6e42ef7de660ff7d9adf2b11651546449c8
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-11-06 18:28:52 -0400

    [skip ci] oops i forgor 💀

--- heck.sh
+++
@@ -1,6 +0,0 @@
-find . -name "*.java" -exec sed -i s/R\.h\./R.i./ {} \; 
-find . -name "*.java" -exec sed -i s/R\.d\./R.e./ {} \; 
-find . -name "*.java" -exec sed -i s/R\.g\./R.h./ {} \;
-find . -name "*.kt" -exec sed -i s/R\.h\./R.i./ {} \; 
-find . -name "*.kt" -exec sed -i s/R\.d\./R.e./ {} \; 
-find . -name "*.kt" -exec sed -i s/R\.g\./R.h./ {} \; 
