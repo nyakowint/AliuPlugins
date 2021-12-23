fce5a289ba9d27358c46d681d6db39ebb2f99e28
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-12-23 02:46:58 -0500

    ASN: maybe fix the server thing

--- AutoServerNotifs/build.gradle.kts
+++ AutoServerNotifs/build.gradle.kts
@@ -1,4 +1,6 @@
-version = "1.0.6"
-description = """Automatically changes notification settings when you join a guild
-""".trimIndent()
-aliucord.excludeFromUpdaterJson.set(true)
+version = "1.0.7"
+description = "Automatically changes notification settings when you join a guild"
+
+aliucord {
+    changelog.set("holy shit i think i fixed it")
+}
--- AutoServerNotifs/src/main/kotlin/me/aniimalz/plugins/AutoServerNotifs.kt
+++ AutoServerNotifs/src/main/kotlin/me/aniimalz/plugins/AutoServerNotifs.kt
@@ -8,6 +8,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.res.ResourcesCompat
+import com.adjust.sdk.Util
 import com.aliucord.Constants
 import com.aliucord.Logger
 import com.aliucord.Utils
@@ -15,6 +16,8 @@ import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.api.SettingsAPI
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.Hook
+import com.aliucord.patcher.after
+import com.aliucord.patcher.before
 import com.aliucord.utils.DimenUtils
 import com.aliucord.utils.ReflectUtils
 import com.aliucord.utils.RxUtils.subscribe
@@ -29,7 +32,9 @@ import com.discord.widgets.guilds.profile.WidgetGuildProfileSheet
 import com.discord.widgets.guilds.profile.WidgetGuildProfileSheetViewModel.TabItems
 import com.lytefast.flexinput.R
 import me.aniimalz.plugins.PluginSettings.Companion.notifFrequency
+import rx.Subscription
 import java.lang.reflect.InvocationTargetException
+import java.text.SimpleDateFormat
 
 @AliucordPlugin
 class AutoServerNotifs : Plugin() {
@@ -43,10 +48,9 @@ class AutoServerNotifs : Plugin() {
 
     private val id = View.generateViewId()
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "SimpleDateFormat")
     override fun start(ctx: Context) {
         bSettings = settings
-        // not yoinked from editserverslocally not at all
         patcher.patch(
             WidgetGuildProfileSheet::class.java.getDeclaredMethod(
                 "configureTabItems",
@@ -97,45 +101,53 @@ class AutoServerNotifs : Plugin() {
             })
 
         // RestAPI.ackGuild - mark as read
-        patcher.patch(
-            StoreGuilds::class.java.getDeclaredMethod(
-                "handleGuildAdd",
-                Guild::class.java
-            ), Hook { // WHY DOES THIS FIRE RANDOMLY WHY
-                if (!settings.getBool("applyAss", true)) return@Hook
-                val guild = GuildWrapper(it.args[0] as Guild)
-                logger.info("${guild.name} (${guild.id}) joined, applying notification settings")
-                logger.info("Suppress @everyone and @here: ${getBool("suppressEveryone")}")
-                logger.info("Suppress Role Mentions: ${getBool("suppressRoles")}")
-                logger.info("Mute ${guild.name}: ${getBool("Mute Guild")}")
-                logger.info("Mobile Push notifs: ${getBool("mobilePushNotifs")}")
-                logger.info("Notification frequency: ${bSettings.notifFrequency.name}")
+        patcher.before<StoreGuilds>("handleGuildAdd", Guild::class.java) {
+            // WHY DOES THIS FIRE RANDOMLY WHY
+            if (!settings.getBool("applyAss", true)) return@before
+            val guild = GuildWrapper(it.args[0] as Guild)
+            if (guilds.containsKey(guild.id)) {
+                Utils.showToast("i think im right on this")
+                return@before
+            }
 
-                Utils.threadPool.execute {
-                    RestAPI.api.updateUserGuildSettings(
-                        guild.id,
-                        RestAPIParams.UserGuildSettings(
-                            getBool("suppressEveryone"),
-                            getBool("suppressRoles"),
-                            getBool("Mute Guild"),
-                            null,
-                            getBool("mobilePushNotifs", true),
-                            bSettings.notifFrequency.value,
-                            null
-                        )
-                    ).subscribe {}
-                }
-
-            })
+            apply(guild)
+        }
 
     }
 
+    private fun apply(guild: GuildWrapper) {
+        Utils.showToast("Applying for ${guild.name}. If you're seeing this without joining a server first then plugin is still broke: oh well")
+        logger.info("${guild.name} (${guild.id}) joined, applying notification settings")
+        logger.info("Suppress @everyone and @here: ${getBool("suppressEveryone")}")
+        logger.info("Suppress Role Mentions: ${getBool("suppressRoles")}")
+        logger.info("Mute ${guild.name}: ${getBool("Mute Guild")}")
+        logger.info("Mobile Push notifs: ${getBool("mobilePushNotifs")}")
+        logger.info("Notification frequency: ${bSettings.notifFrequency.name}")
+
+        Utils.threadPool.execute {
+            sub = RestAPI.api.updateUserGuildSettings(
+                guild.id,
+                RestAPIParams.UserGuildSettings(
+                    getBool("suppressEveryone"),
+                    getBool("suppressRoles"),
+                    getBool("Mute Guild"),
+                    null,
+                    getBool("mobilePushNotifs", true),
+                    bSettings.notifFrequency.value,
+                    null
+                )
+            ).subscribe {}
+        }
+    }
+
+    private var sub: Subscription? = null
     private fun getBool(setting: String, default: Boolean = false): Boolean {
         return bSettings.getBool(setting, default)
     }
 
     override fun stop(context: Context) {
         patcher.unpatchAll()
+        sub?.unsubscribe()
     }
 
     companion object {
