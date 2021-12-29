432cf2f3c1375035f158e90d23a4250dcd86c4c5
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-12-28 23:45:43 -0500

    bruh

--- FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
+++ FreeNitro/src/main/kotlin/me/aniimalz/plugins/FreeNitro.kt
@@ -7,13 +7,6 @@ import com.lytefast.flexinput.R
 
 @AliucordPlugin
 class FreeNitro : Plugin() {
-    init {
-        settingsTab = SettingsTab(
-            PluginSettings::class.java,
-            SettingsTab.Type.BOTTOM_SHEET
-        ).withArgs(settings)
-    }
-
     override fun start(ctx: Context) {
 
     }
