e00da253869f21608b8b8e7e140e2fef9de5e740
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2022-02-06 00:56:45 -0500

    version

--- Fart/build.gradle.kts
+++ Fart/build.gradle.kts
@@ -1,9 +1,9 @@
-version = "1.3.0"
+version = "1.3.1"
 description = "fart"
 
 aliucord {
     changelog.set("""
-        # 1.3.0
+        # 1.3.1
         You can now fart in amazing 4k with sound
     """.trimIndent())
 }
