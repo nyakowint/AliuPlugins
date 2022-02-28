1ce516dcbb395cc722993068c0158b74ff1dffa9
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2022-02-28 16:33:14 -0500

    fix auto revealing spoiler emotes

--- AutoShowSpoilers/build.gradle.kts
+++ AutoShowSpoilers/build.gradle.kts
@@ -1,2 +1,2 @@
-version = "1.0.4"
+version = "1.1.0"
 description = "Automatically clicks on spoilers for you"
--- AutoShowSpoilers/src/main/kotlin/me/aniimalz/plugins/AutoShowSpoilers.kt
+++ AutoShowSpoilers/src/main/kotlin/me/aniimalz/plugins/AutoShowSpoilers.kt
@@ -5,6 +5,8 @@ import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.InsteadHook
 import com.aliucord.patcher.after
+import com.discord.simpleast.core.node.Node
+import com.discord.utilities.textprocessing.MessagePreprocessor
 import com.discord.utilities.textprocessing.node.SpoilerNode
 import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemAttachment
 import com.discord.widgets.chat.list.entries.AttachmentEntry
@@ -12,6 +14,11 @@ import com.discord.widgets.chat.list.entries.AttachmentEntry
 @AliucordPlugin
 class AutoShowSpoilers : Plugin() {
     override fun start(ctx: Context) {
+        patcher.after<MessagePreprocessor>("processNode", Node::class.java) {
+            val node = it.args[0]
+            if (node !is SpoilerNode<*>) return@after
+            node.updateState(spoilers.size, true)
+        }
         patcher.after<SpoilerNode<*>>("isRevealed") { this.isRevealed = true }
 
         // Embed spoiler patch
